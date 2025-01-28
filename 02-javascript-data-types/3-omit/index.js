9c10ee599f9597a6828b276231751a43d56c05d6
George Albegov <gondza2828@gmail.com>
2025-01-29 00:10:55 +0300

    решение задачи omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    const resultObj = {};
 
+    Object.entries(obj).forEach(([key, value]) => {
+        if (!fields.includes(key)) {
+          resultObj[key] = value;
+        }
+    });
+
+    return resultObj;
 };
