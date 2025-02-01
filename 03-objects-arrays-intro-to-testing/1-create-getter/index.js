c85e9522da8b60a4d148a75814c17e740ae1f80f
George Albegov <gondza2828@gmail.com>
2025-02-01 18:53:16 +0300

    Решение задачи uniq

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -6,14 +6,10 @@
 export function createGetter(path) {
     const fields = path.split('.');
 
-    return obj => {
+    return function (obj) {
         if (Object.keys(obj).length === 0) return undefined;
-
-        fields.forEach(field => {
-            const keys = Object.keys(obj);
-            obj = keys.includes(field) ? obj[field] : undefined;
-        })
-
-        return obj;
+        return fields.reduce((acc, part) => {
+            return acc && acc[part];
+        }, obj);
     };
 }
