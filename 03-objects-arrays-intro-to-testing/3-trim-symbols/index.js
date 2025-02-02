9aa36063a4c012eb0732257906d684cc01728955
George Albegov <gondza2828@gmail.com>
2025-02-02 12:42:52 +0300

    Исправление проверки крайних значений в trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,12 +5,9 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-    if (size == 0) {
-        return '';
-    } else if (!size) {
-        return string;
-    }
-
+    if (size == 0) return '';
+    if (!size) return string;
+    
     let result = '';
     let count = 0;
     let prevChar = '';
